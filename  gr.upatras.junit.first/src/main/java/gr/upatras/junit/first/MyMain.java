c68abf5ef52900912b25ea8fb61b11cd74e43073
up1056586 <up1056586@yk2-pc52.ece.upatras.gr>
2023-03-31 09:56:48 +0300

    Commit message

---
+++  gr.upatras.junit.first/src/main/java/gr/upatras/junit/first/MyMain.java
@@ -0,0 +1,18 @@
+package gr.upatras.junit.first;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClass m = new MyClass();
+		int result = m.multiply(10, 5);
+		System.out.println("result is "+ result);
+
+	}
+
+	public Object multiply(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
